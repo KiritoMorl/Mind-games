47c96206458cb74bb0f66bddf6a7e357556b4414
Kurbonov P <kroritord@bk.ru>
2022-12-09 20:34:59 +0300

    fix even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,19 +1,20 @@
-import { engine, getRandomArbitrary } from '../index.js';
+import engine from '../index.js';
+import getRandomArbitrary from '../utils.js';
 
-const task = 'Answer "yes" if the number is even, otherwise answer "no".';
+const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 const getCorrectAnswer = (randomNum) => {
   if (randomNum % 2 === 0) {
     return 'yes';
   }
   return 'no';
 };
-const rules = () => {
+const generateRound = () => {
   const minNumber = 1;
   const maxNumber = 100;
   const randomNumber = getRandomArbitrary(minNumber, maxNumber);
   const CorrectAnswer = getCorrectAnswer(randomNumber);
   return [randomNumber, CorrectAnswer];
 };
-const even = () => engine(task, rules);
+const runEvenGame = () => engine(rules, generateRound);
 
-export default even;
+export default runEvenGame;
