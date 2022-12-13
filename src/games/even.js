4ec087273556ef96042f392ef9e42b3eb2628889
Kurbonov P <kroritord@bk.ru>
2022-12-13 23:45:07 +0300

    fix even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,20 +1,21 @@
+import _ from 'lodash';
 import engine from '../index.js';
-import getRandomArbitrary from '../utils.js';
 
 const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
+
 const getCorrectAnswer = (randomNum) => {
   if (randomNum % 2 === 0) {
     return 'yes';
   }
   return 'no';
 };
+
 const generateRound = () => {
-  const minNumber = 1;
-  const maxNumber = 100;
-  const randomNumber = getRandomArbitrary(minNumber, maxNumber);
+  const randomNumber = _.random(1, 100);
   const CorrectAnswer = getCorrectAnswer(randomNumber);
   return [randomNumber, CorrectAnswer];
 };
+
 const runEvenGame = () => engine(rules, generateRound);
 
 export default runEvenGame;
