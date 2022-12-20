34a1a059d66e9ef7ddd1ac6103e0e9748ede031b
Kurbonov P <kroritord@bk.ru>
2022-12-21 00:11:18 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 import readlineSync from 'readline-sync';
 
 const roundsCount = 3;
+
 const engine = (rules, generateRound) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(rules);
+
   for (let i = 0; i < roundsCount; i += 1) {
-    const [question, correctAnswer] = generateRound();
+    const [question, answer] = generateRound();
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    if (correctAnswer === userAnswer) {
+    if (answer === userAnswer) {
       console.log('Correct!');
     } else {
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${answer}.`);
       console.log(`Let's try again, ${userName}!`);
       return;
     }
   }
+
   console.log(`Congratulations, ${userName}!`);
 };
 
