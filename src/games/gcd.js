189cb45b26a4c615f30ccc5a0b9ff0383af5a360
Kurbonov P <kroritord@bk.ru>
2022-12-22 22:34:25 +0300

    fix gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,13 +1,16 @@
-import _ from 'lodash';
+import random from 'lodash/random.js';
 import engine from '../index.js';
 
 const rules = 'Find the greatest common divisor of given numbers.';
 
-const getGcd = (num, num2) => { if (num === 0) { return num2; } return getGcd(num2 % num, num); };
+const getGcd = (num, num2) => {
+  if (num === 0) { return num2; }
+  return getGcd(num2 % num, num);
+};
 
 const generateRound = () => {
-  const randomNumber1 = _.random(1, 100);
-  const randomNumber2 = _.random(1, 100);
+  const randomNumber1 = random(1, 100);
+  const randomNumber2 = random(1, 100);
   const question = `${randomNumber1} ${randomNumber2}`;
   const answer = String(getGcd(randomNumber1, randomNumber2));
   return [question, answer];
